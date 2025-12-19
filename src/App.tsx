52688ef00c317130574acf1e280db758995444fc
pangil5634 <oksk6681@naver.com>
2025-12-19 13:29:48 +0900

    feat : add custon fonts and styles

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { wickedMouseStyle } from "./styles/font";
+
+function App() {
+  return (
+    <div>
+      <h1 style={wickedMouseStyle}>CHECKY</h1>
+      <span>good</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/font.ts
@@ -0,0 +1,3 @@
+export const wickedMouseStyle = {
+  fontFamily: "WickedMouse",
+} as const;
