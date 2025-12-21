e1a2928a089903b61e44370719a80109597a854b
pangil5634 <oksk6681@naver.com>
2025-12-21 13:28:42 +0900

    fix : apply tailwind css in all files

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,11 @@
 function LoginPage() {
   return (
     <div>
-      <p>LoginPage</p>
+      <p>CHECKY</p>
+      <p className="text-sm tracking-tight opacity-80 text-red-500">
+        어제 보다 더 나은 오늘을 위해
+      </p>
+      <button>시작하기</button>
     </div>
   );
 }
