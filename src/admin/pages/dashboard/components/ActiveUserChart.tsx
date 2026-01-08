edb47b5ab9a5600cda2d03644e2a636a53e68a01
pangil5634 <oksk6681@naver.com>
2026-01-08 23:21:21 +0900

    style : update border color in chart

--- src/admin/pages/dashboard/components/ActiveUserChart.tsx
+++ src/admin/pages/dashboard/components/ActiveUserChart.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 function ActiveUserChart({ data }: Props) {
   return (
-    <div className="w-full h-65 bg-white border rounded-lg p-4  pb-8">
+    <div className="w-full h-65 bg-white border rounded-lg p-4  pb-8 border-gray-200 ">
       <p className="font-semibold mb-3">활성 사용자 추이</p>
       <ResponsiveContainer width="100%" height="100%">
         <LineChart data={data}>
