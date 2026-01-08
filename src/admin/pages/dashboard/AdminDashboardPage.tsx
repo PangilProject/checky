8cada0e32aa84024284124f0b6cfdb6eb4c9e4a8
pangil5634 <oksk6681@naver.com>
2026-01-08 23:17:09 +0900

    feat : update the gird to flex style

--- src/admin/pages/dashboard/AdminDashboardPage.tsx
+++ src/admin/pages/dashboard/AdminDashboardPage.tsx
@@ -19,15 +19,25 @@ function AdminDashboardPage() {
 
       <DashboardGrid stats={stats} />
 
-      <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
-        <SectionTitle title="가입자 추이" description="최근 가입자 변화 추이" />
-        <UserSignupChart data={stats.signupByDate} />
+      {/* 차트 섹션 */}
+      <div className="flex flex-col xl:flex-row gap-6">
+        {/* 가입자 추이 */}
+        <div className="flex-1">
+          <SectionTitle
+            title="가입자 추이"
+            description="최근 가입자 변화 추이"
+          />
+          <UserSignupChart data={stats.signupByDate} />
+        </div>
 
-        <SectionTitle
-          title="활성 사용자 추이"
-          description="최근 7일 로그인 기준"
-        />
-        <ActiveUserChart data={stats.activeByDate} />
+        {/* 활성 사용자 추이 */}
+        <div className="flex-1">
+          <SectionTitle
+            title="활성 사용자 추이"
+            description="최근 7일 로그인 기준"
+          />
+          <ActiveUserChart data={stats.activeByDate} />
+        </div>
       </div>
 
       <Space10 direction="mb" />
